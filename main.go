openshift/multiarch-tuning-operator

MULTIARCH-4974: Added preferredDuringSchedulingIgnoredDuringExecution during scheduling using append method

main.go
@@ -40,6 +40,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
@@ -113,10 +114,17 @@ func main() {
 	}
 	if enableClusterPodPlacementConfigOperandControllers {
 		leaderId = fmt.Sprintf("ppc-controllers-%s", leaderId)
+
+		// Set a default field selector, but ensure ClusterPodPlacementConfig overrides it
 		// We need to watch the pods with the status.phase equal to Pending to be able to update the nodeAffinity.
 		// We can discard the other pods because they are already scheduled.
-		cacheOpts.DefaultFieldSelector = fields.OneTermEqualSelector("status.phase", "Pending")
+		cacheOpts.ByObject = map[client.Object]cache.ByObject{
+			&corev1.Pod{}: {
+				Field: fields.OneTermEqualSelector("status.phase", "Pending"),
+			},
+		}
 	}
+
 	// Rapid Reset CVEs. For more information see:
 	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
 	// - https://github.com/advisories/GHSA-4374-p667-p6c8
@@ -189,15 +197,6 @@ func RunOperator(mgr ctrl.Manager) {
 	config := ctrl.GetConfigOrDie()
 	clientset := kubernetes.NewForConfigOrDie(config)
 
-	if enableCPPCInformer {
-		cppcSyncer := podplacement.NewCPPCSyncer(mgr)
-		if err := mgr.Add(cppcSyncer); err != nil {
-			setupLog.Error(err, "unable to add CPPCSyncer")
-			os.Exit(1)
-		}
-		setupLog.Info("CPPCSyncer is enabled")
-	}
-
 	// Get GVK for ClusterPodPlacementConfig
 	gvk, _ := apiutil.GVKForObject(&multiarchv1beta1.ClusterPodPlacementConfig{}, mgr.GetScheme())
 
@@ -229,6 +228,15 @@ func RunClusterPodPlacementConfigOperandControllers(mgr ctrl.Manager) {
 		Recorder:  mgr.GetEventRecorderFor(utils.OperatorName),
 	}).SetupWithManager(mgr),
 		unableToCreateController, controllerKey, "PodReconciler")
+
+	if enableCPPCInformer {
+		cppcSyncer := podplacement.NewCPPCSyncer(mgr)
+		if err := mgr.Add(cppcSyncer); err != nil {
+			setupLog.Error(err, "unable to add CPPCSyncer")
+			os.Exit(1)
+		}
+		setupLog.Info("CPPCSyncer is enabled")
+	}
 
 	must(mgr.Add(podplacement.NewGlobalPullSecretSyncer(clientset, globalPullSecretNamespace, globalPullSecretName)),
 		unableToAddRunnable, runnableKey, "GlobalPullSecretSyncer")

AnnaZivkovic/multiarch-manager-operator

main.go
@@ -40,6 +40,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
@@ -114,10 +115,17 @@ func main() {
 	}
 	if enableClusterPodPlacementConfigOperandControllers {
 		leaderId = fmt.Sprintf("ppc-controllers-%s", leaderId)
+
+		// Set a default field selector, but ensure ClusterPodPlacementConfig overrides it
 		// We need to watch the pods with the status.phase equal to Pending to be able to update the nodeAffinity.
 		// We can discard the other pods because they are already scheduled.
-		cacheOpts.DefaultFieldSelector = fields.OneTermEqualSelector("status.phase", "Pending")
+		cacheOpts.ByObject = map[client.Object]cache.ByObject{
+			&corev1.Pod{}: {
+				Field: fields.OneTermEqualSelector("status.phase", "Pending"),
+			},
+		}
 	}
+
 	// Rapid Reset CVEs. For more information see:
 	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
 	// - https://github.com/advisories/GHSA-4374-p667-p6c8
@@ -190,15 +198,6 @@ func RunOperator(mgr ctrl.Manager) {
 	config := ctrl.GetConfigOrDie()
 	clientset := kubernetes.NewForConfigOrDie(config)
 
-	if enableCPPCInformer {
-		cppcSyncer := clusterpodplacementconfig.NewCPPCSyncer(mgr)
-		if err := mgr.Add(cppcSyncer); err != nil {
-			setupLog.Error(err, "unable to add CPPCSyncer")
-			os.Exit(1)
-		}
-		setupLog.Info("CPPCSyncer is enabled")
-	}
-
 	// Get GVK for ClusterPodPlacementConfig
 	gvk, _ := apiutil.GVKForObject(&multiarchv1beta1.ClusterPodPlacementConfig{}, mgr.GetScheme())
 
@@ -230,6 +229,15 @@ func RunClusterPodPlacementConfigOperandControllers(mgr ctrl.Manager) {
 		Recorder:  mgr.GetEventRecorderFor(utils.OperatorName),
 	}).SetupWithManager(mgr),
 		unableToCreateController, controllerKey, "PodReconciler")
+
+	if enableCPPCInformer {
+		cppcSyncer := clusterpodplacementconfig.NewCPPCSyncer(mgr)
+		if err := mgr.Add(cppcSyncer); err != nil {
+			setupLog.Error(err, "unable to add CPPCSyncer")
+			os.Exit(1)
+		}
+		setupLog.Info("CPPCSyncer is enabled")
+	}
 
 	must(mgr.Add(podplacement.NewGlobalPullSecretSyncer(clientset, globalPullSecretNamespace, globalPullSecretName)),
 		unableToAddRunnable, runnableKey, "GlobalPullSecretSyncer")